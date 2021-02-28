f86afc0d69a6a18b0576dbbb5358feb61279a4e1
MANAS MISHRA <f20170546@pilani.bits-pilani.ac.in>
2021-03-01 00:15:33 +0530

    updated symbols

--- es6/symbols.js
+++ es6/symbols.js
@@ -5,8 +5,26 @@ console.log(typeof s1)
 s2 = Symbol("Description")
 console.log(s2.toString())
 
+console.log('')
 s3 = Symbol("Description")
 s4 = Symbol()
 console.log(s1 === s4)
-console.log(s2 === s3)
 console.log(s2 == s3)
+console.log('')
+
+// Add symbol to global registry
+s5 = Symbol.for('Key1')
+s6 = Symbol.for('Key1')
+console.log(s5 == s6)
+console.log(Symbol.keyFor(s5))
+console.log('')
+
+// Uses
+let fname = Symbol('FirstName')
+let person = {
+  [fname]: "Manas"
+}
+
+console.log(Object.getOwnPropertyNames(person))
+console.log(Object.getOwnPropertySymbols(person))
+console.log(person[fname])
