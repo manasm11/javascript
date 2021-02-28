535cdf15adbbe9dd48d94ae8d1e6e9c696a806ac
MANAS MISHRA <f20170546@pilani.bits-pilani.ac.in>
2021-02-28 23:37:24 +0530

    added maps

--- es6/maps.js
+++ es6/maps.js
@@ -2,6 +2,23 @@
 m = new Map()
 m.set('fname', "Manas")
 m.set({}, true)
+m.set(true, "Sahi me !!!")
 console.log(m.size)
-m.set({}, false)
-console.log(m.get({}))
+console.log(m.get('fname'))
+console.log(m.has('fname'))
+m.clear();
+console.log(m.size)
+
+// MISC
+console.log(m.size)
+o1 = {}
+o2 = {}
+m.set(o1, "Hello")
+m.set(o2, "Hello")
+console.log(m.get(o1))
+
+h1 = "HELLO"
+h2 = "HELLO"
+m.set(h1, "heallo")
+m.set(h2, "HOLA")
+console.log(m.get(h1))
